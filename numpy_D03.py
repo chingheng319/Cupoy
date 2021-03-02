3bcb35e985230ff03e1f80edf0b63d46c4ae1157
chingheng319 <78397356+chingheng319@users.noreply.github.com>
2021-03-02 21:34:18 +0800

    Add files via upload

--- numpy_D03.py
+++ numpy_D03.py
@@ -1,5 +1,9 @@
 import numpy as np
-v = (2000/20)
-print(v)
-z = np.log20p(x)
-print(z)
+v1 = 20000
+v0 = 20
+v2 = 20*np.log10(v1/v0)
+print(v2)
+
+v130 = 10**(30/20)*v0
+v150 = 10**(50/20)*v0
+print(v130/v150)
