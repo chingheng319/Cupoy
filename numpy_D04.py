4ab56ac7638b8810b2e29fa2be2f0803331ed874
chingheng319 <78397356+chingheng319@users.noreply.github.com>
2021-03-03 11:39:05 +0800

    Add files via upload

---
+++ numpy_D04.py
@@ -0,0 +1,6 @@
+import numpy as np
+e_score = np.array([55,89,76,65,48,70])
+m_score = np.array([60,85,60,68,55,60])
+c_score = np.array([65,90,82,72,66,77])
+c1 = np.greater(e_score,m_score)
+print(c1)
