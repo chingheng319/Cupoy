52c26a852a5cf4f671423a0e0ffab86f5b5a20d3
chingheng319 <78397356+chingheng319@users.noreply.github.com>
2021-03-23 15:09:38 +0800

    Add files via upload

---
+++ numpy_D05.py
@@ -0,0 +1,13 @@
+import numpy as np
+e_score = np.array([55,89,76,65,48,70])
+m_score = np.array([60,85,60,68,55,60])
+c_score = np.array([65,90,82,72,66,77])
+c1 = np.greater(e_score,m_score)
+print(c1)
+print(np.sum(c1))
+c2 = np.greater(c_score,m_score)
+c3 = np.greater(c_score,e_score)
+print(c2)
+print(c3)
+print(np.logical_and(c2,c3))
+print(np.logical_and(c2,c3).any())
